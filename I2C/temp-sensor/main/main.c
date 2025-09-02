a3aa3ec180c0a05f69dfb4eab6a663fbf3de9adc
deveranthony <deveranthony5@gmail.com>
2025-09-01 13:57:15 -1100

    Update main.c
    Adding library

--- I2C/temp-sensor/main/main.c
+++ I2C/temp-sensor/main/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_types.h"
+#include "driver/i2c.h"
+
+#define SDA_GPIO 26
+#define SCL_GPIO 25
+#define Address 
 
 void app_main(void)
 {
