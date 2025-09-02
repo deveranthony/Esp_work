367dcae6b94477838eb8513a06ebb41b9180e2d2
deveranthony <deveranthony5@gmail.com>
2025-09-01 14:18:25 -1100

    This is first change

--- I2C/temp-sensor/main/main.c
+++ I2C/temp-sensor/main/main.c
@@ -6,9 +6,28 @@
 
 #define SDA_GPIO 26
 #define SCL_GPIO 25
-#define Address 
+#define Address 0x48
 
 void app_main(void)
 {
+    i2c_config_t i2c_config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = SDA_GPIO,
+        .scl_io_num = SCL_GPIO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 100000
+    };
+    i2c_param_config(I2C_NUM_0, &i2c_config);
+    i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0,0,0);
 
+    uint8_t raw[2];
+    i2c_cmd_handle_t cmd_handle = i2c_cmd_link_create();
+    i2c_master_start(cmd_handle);
+
+    i2c_master_write_byte(cmd_handle, (Address << 1)|I2C_MASTER_READ, true);
+    i2c_master_read(cmd_handle, (uint8_t*)&raw, 2, I2C_MASTER_ACK);
+    i2c_master_stop(cmd_handle);
+    i2c_master_cmd_begin(I2C_NUM_0, cmd_handle, 1000/portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd_handle);
 }
